d7250972859c2983d7b6d14e0b8c00c50ee8d9c7
Shubh-Pawar007 <shubhampawar242767@gmail.com>
2022-04-11 13:15:47 +0530

    latest Changes

--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -9,7 +9,20 @@ console.log(data)
         <>
         <h1>Groceries</h1>
         <div className="container">
-            {/* map through the data and display the cards */}
+            
+            {data.map(item=>{
+                return (
+                    <>
+                        <div className="Gross" style={{border:'2px solid black',height:'350px', width:'400px'}} >
+                           <img src={item.imgURL}></img>
+                           <h4>{item.title}</h4>
+                           <h5> {item.mrp}</h5>
+                           <h4>{item.sellingPrice}</h4>
+                           
+                        </div>
+                    </>
+                )
+            })}
         </div>
         </>
     )
